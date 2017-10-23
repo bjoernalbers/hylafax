c9fef589ad73a01d235822d62e55541eb2c4e8b3
Björn Albers <bjoernalbers@gmail.com>
2017-10-23 18:52:53 +0200

    Bump version (0.3.0).

--- spec/hylafax_spec.rb
+++ spec/hylafax_spec.rb
@@ -2,6 +2,6 @@ require 'spec_helper'
 
 RSpec.describe HylaFAX do
   it 'has a version number' do
-    expect(described_class::VERSION).to eq '0.2.1'
+    expect(described_class::VERSION).to eq '0.3.0'
   end
 end
